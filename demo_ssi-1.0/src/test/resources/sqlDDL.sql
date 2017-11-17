a5d9a7e055ebcd22319cf70a4a7c7524b3bc2d3a
Spole0168 <Spole_168@qq.com>
2017-11-17 15:20:57 +0800

    Date:2017-11-17 15:20:54

--- demo_ssi-1.0/src/test/resources/sqlDDL.sql
+++ demo_ssi-1.0/src/test/resources/sqlDDL.sql
@@ -20,11 +20,11 @@ create table student (
 drop table if exists t_user;    	
 create table t_user (    	
     id varchar(40) not null comment '主键',
-    username varchar(20) null comment '用户登录名 唯一性',
+    username varchar(50) null comment '用户登录名 唯一性',
     password varchar(500) null comment '用户密码',
     tel varchar(15) null comment '手机号  唯一性',
     email varchar(60) null comment '用户邮箱  唯一性',
-    name varchar(20) null comment '用户名',
+    name varchar(50) null comment '用户名',
     
     is_valid char(1) null default '0' comment '是否有效 0-有效，1-删除 ',
     creator varchar(20) comment '创建者',
@@ -38,11 +38,11 @@ drop table if exists t_menu;
 create table t_menu (    	
     id varchar(40) not null comment '主键',
     
+    menu_pid varchar(40) null comment '父级菜单id',
     menu_name varchar(60) null comment '菜单名称',
     menu_code varchar(20) null comment '菜单编码   唯一性',
     menu_type varchar(50) null comment '菜单类型',
     menu_level int null comment '菜单级别 1-一级菜单 2-二级菜单 3-三级菜单',
-    menu_pcode varchar(20) null comment '菜单父级编码',
     menu_url varchar(50) null comment '菜单连接地址 url',
     menu_order varchar(2) null comment '菜单排序',
     
@@ -76,8 +76,8 @@ drop table if exists t_role_menus;
 create table t_role_menus (    	
     id varchar(40) not null comment '主键',
     
-    role_code varchar(20) null comment '角色编码',
-    menu_code varchar(20) null comment '菜单编码',
+    role_id varchar(40) null comment '角色id',
+    menu_id varchar(40) null comment '菜单编码',
     
     is_valid char(1) null default '0' comment '是否有效 0-有效，1-删除 ',
     creator varchar(20) comment '创建者',
@@ -92,7 +92,7 @@ create table t_user_roles (
     id varchar(40) not null comment '主键',
     
     username varchar(20) null comment '用户登录名',
-    role_code varchar(20) null comment '角色编码',
+    role_id varchar(40) null comment '角色id',
     
     is_valid char(1) null default '0' comment '是否有效 0-有效，1-删除 ',
     creator varchar(20) comment '创建者',
